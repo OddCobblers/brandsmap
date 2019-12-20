0d7d8e221121e988617fda4e585ed0ab59cc63d6
OddCobblers <59093663+OddCobblers@users.noreply.github.com>
2019-12-20 09:59:35 -0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1YXX-vzREkpuK1mbFGoRvu0Vt-X4-ZXN-2npmJSKhNd8/edit#gid=0';
